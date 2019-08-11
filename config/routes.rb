8346db0c174baeabd5f2da353fefae1448eb106a
Chase Jensen <soccerchase@gmail.com>
2019-08-11 06:07:16 -0600

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/survey', to: 'static#survey'
+  root to: 'static#site1', as: 'books'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
