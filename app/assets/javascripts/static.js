6f502a34fe8d23fb08a6c74945d4927e4d94864c
Chase Jensen <soccerchase@gmail.com>
2019-08-11 06:08:02 -0600

    Add JS

---
+++ app/assets/javascripts/static.js
@@ -0,0 +1,5 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+
+  // <img src="http://www.harmless.com/img" width="400" height="400" onmouseover="..." />%
